986de3df6afc620c983175584f205104dcff46e1
Ubuntu <ubuntu@ip-10-164-62-121.ec2.internal>
2013-07-07 23:47:41 +0000

    added read index file

--- web.js
+++ web.js
@@ -1,12 +1,14 @@
 var express = require('express');
+var fs =  require('fs');
 
 var app = express.createServer(express.logger());
 
 app.get('/', function(request, response) {
 
-  var buffer = new Buffer("Hello World 3", "utf-8")
+    
+  var buffer = fs.readFileSync("index.html", "utf-8");
 
-   response.send(buffer);
+   response.send(buffer.toString());
   //response.send('Hello World2!');
 });
 
