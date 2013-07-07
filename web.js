71d21a70f5a288e48319767a4853ec1bc6495b8a
Ubuntu <ubuntu@ip-10-164-62-121.ec2.internal>
2013-07-07 23:39:24 +0000

    added buffer

--- web.js
+++ web.js
@@ -3,7 +3,11 @@ var express = require('express');
 var app = express.createServer(express.logger());
 
 app.get('/', function(request, response) {
-  response.send('Hello World2!');
+
+  var buffer = new Buffer("Hello World 3", "utf-8")
+
+   response.send(buffer);
+  //response.send('Hello World2!');
 });
 
 var port = process.env.PORT || 5000;
